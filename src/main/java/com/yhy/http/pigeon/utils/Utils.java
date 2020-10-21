0bd9ba8f2fa4450a5c3a67f49f63dd6a5e81d8d3
颜洪毅 <yhyzgn@gmail.com>
2020-10-21 19:48:58 +0800

    exclude

--- src/main/java/com/yhy/http/pigeon/utils/Utils.java
+++ src/main/java/com/yhy/http/pigeon/utils/Utils.java
@@ -18,7 +18,7 @@ import java.util.*;
  */
 public class Utils {
     private static final Type[] EMPTY_TYPE_ARRAY = new Type[0];
-    public final static String VERSION = "1.2.12";
+    public final static String VERSION = "1.2.13";
 
     public static boolean isEmpty(Object object) {
         if (null == object) return true;
