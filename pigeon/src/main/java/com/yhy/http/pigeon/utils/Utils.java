7376d15c69104a8cbbb1fb1c7b1f45c0c7510b04
yhyzgn <yhyzgn@gmail.com>
2021-11-26 00:58:25 +0800

    update

--- pigeon/src/main/java/com/yhy/http/pigeon/utils/Utils.java
+++ pigeon/src/main/java/com/yhy/http/pigeon/utils/Utils.java
@@ -18,7 +18,7 @@ import java.util.*;
  */
 public class Utils {
     private static final Type[] EMPTY_TYPE_ARRAY = new Type[0];
-    public final static String VERSION = "1.5.6";
+    public final static String VERSION = "1.5.7";
 
     public static boolean isEmpty(Object object) {
         if (null == object) return true;
