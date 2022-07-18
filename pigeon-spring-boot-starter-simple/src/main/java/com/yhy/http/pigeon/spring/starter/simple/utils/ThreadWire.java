5b831aebfbe7fb259a29ec0f7b517f1ef362e5b4
yhyzgn <yhyzgn@gmail.com>
2022-07-18 20:59:39 +0800

    utils

---
+++ pigeon-spring-boot-starter-simple/src/main/java/com/yhy/http/pigeon/spring/starter/simple/utils/ThreadWire.java
@@ -0,0 +1,58 @@
+package com.yhy.http.pigeon.spring.starter.simple.utils;
+
+import java.lang.ref.WeakReference;
+
+public class ThreadWire<T> {
+    // 弱引用的ThreadLocal
+    private WeakReference<ThreadLocal<T>> ref;
+
+    /**
+     * 无参数构造方法
+     */
+    public ThreadWire() {
+        ref = new WeakReference<>(new ThreadLocal<>());
+    }
+
+    /**
+     * 带初始值的构造方法
+     *
+     * @param t 初始值
+     */
+    public ThreadWire(T t) {
+        ref = new WeakReference<>(ThreadLocal.withInitial(() -> t));
+    }
+
+    /**
+     * 设置值
+     *
+     * @param t 当前线程下的值
+     */
+    public void set(T t) {
+        ThreadLocal<T> local = ref.get();
+        if (null == local) {
+            local = new ThreadLocal<>();
+        }
+        local.set(t);
+        ref = new WeakReference<>(local);
+    }
+
+    /**
+     * 获取值
+     *
+     * @return 当前线程下的值
+     */
+    public T get() {
+        ThreadLocal<T> local = ref.get();
+        return null != local ? local.get() : null;
+    }
+
+    /**
+     * 移除资源
+     */
+    public void remove() {
+        ThreadLocal<T> local = ref.get();
+        if (null != local) {
+            local.remove();
+        }
+    }
+}
